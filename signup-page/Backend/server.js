099fdb25625c054e0ba3d65d37290210f57553b9
Nicolette Steenkamp <steenkampnicolette815@gmail.com>
2024-10-01 09:25:48 +0200

    changes

--- signup-page/Backend/server.js
+++ signup-page/Backend/server.js
@@ -128,3 +128,4 @@ app.get('/api/protected', verifyToken, (req, res) => {
 // Start server
 const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
