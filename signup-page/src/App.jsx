db1ca5a89ec8975a01e35dab1d13bf8a05012459
Nicolette Steenkamp <steenkampnicolette815@gmail.com>
2024-10-01 12:44:44 +0200

    edited

--- signup-page/src/App.jsx
+++ signup-page/src/App.jsx
@@ -1,18 +1,21 @@
 import RegisterForm from './Register'; 
 import Home from './Home';
 import LoginForm from './Login';
+import NotFound from './NotFound'; 
 
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
+     
       <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
         <Route path="/home" element={<Home />} />              
         <Route path="/register" element={<RegisterForm />} /> 
         <Route path="/login" element={<LoginForm />} />  
-        
+        <Route path="*" element={<NotFound />} /> 
       </Routes>
     </BrowserRouter>
   );
